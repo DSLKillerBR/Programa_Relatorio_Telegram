587251214f74ebe89505ba17af2a4c1de70cf7d5
Breno Pinheiro de Oliveira <62961937+DSLKillerBR@users.noreply.github.com>
2022-03-12 17:47:54 -0300

    Create relatorios_wpp.java
    Criando o programa na linguagem Java.

---
+++ relatorios_wpp.java
@@ -0,0 +1,25 @@
+/******************************************************************************
+        AUTOR: BRENO PINHEIRO DE OLIVEIRA
+
+        PROGRAMA PARA GERAÇÃO DE MENSAGENS AUTOMÁTICAS PARA UTILIZAR NO WHATSAPP
+
+
+                    O programa não possui fim comercial, apenas para
+                    uso pessoal. O mesmo gera um arquivo com um padrão
+                    de mensagens que usarei para conseguir acompanhar
+                    negociações que faço utilizando Criptomoedas.
+
+
+*******************************************************************************/
+
+
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("\n---------------------------------------------------\n \n \n ");
+
+    System.out.println("PROGRAMA DE GERADOR DE RELATÓRIOS PARA WHATSAPP");
+
+    System.out.println("\n \n \n--------------------------------------------------- \n \n\n");
+  }
+}
