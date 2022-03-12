ed1aa1dc932180019c02abbe51f68cc464566dcf
Breno Pinheiro de Oliveira <62961937+DSLKillerBR@users.noreply.github.com>
2022-03-12 19:05:38 -0300

    Update relatorios_wpp.java
    Adicionando mais algumas funções do programa em Java. Como não tenho muito conhecimento da linguagem, este programa vai ser praticamente uma cópia da versão em C, porém em Java.

--- relatorios_wpp.java
+++ relatorios_wpp.java
@@ -12,7 +12,7 @@
 
 *******************************************************************************/
 
-
+import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
@@ -21,5 +21,21 @@ class Main {
     System.out.println("PROGRAMA DE GERADOR DE RELATÓRIOS PARA WHATSAPP");
 
     System.out.println("\n \n \n--------------------------------------------------- \n \n\n");
+
+    // criando as variáveis que serão utilizadas. Todas estão definidas como double para que possamos utilizar as casas decimais
+    double dolar = 0, lucro_total_dolar = 0, lucro_trade_dolar = 0, lucro_total_real = 0, lucro_trade_real = 0, porcentagem_lucro =0, alavancagem =0, valor_moeda_compra =0, quantidade_negociada = 0, valor_moeda_venda = 0;
+
+
+    System.out.println("\n Digite o valor da compra:  ");
+    Scanner scanner = new Scanner(System.in); // solicitando ao usuário que ele digite a data que fez a compra
+    double valor_inicial = scanner.nextDouble();
+
+
+    System.out.println("\n Digite o valor da venda:  ");
+
+    double valor_final = scanner.nextDouble ();// solicitando ao usuário que ele informe a data que fez a venda das moedas
+     System.out.println("\n Valor da compra:  " + valor_inicial) ;
+
+    System.out.println("\n Valor da venda:  " + valor_final);
   }
 }
